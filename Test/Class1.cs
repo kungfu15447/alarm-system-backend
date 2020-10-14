acc4a2cc974f49e91911321a1704cad09ef58f20
Frederik Jensen <frederikhj0@gmail.com>
2020-10-14 14:18:35 +0200

    Added Test folder and moved functions into a src folder

---
+++ Test/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest() {
+            Assert.Equal(4, 4);
+        }
+    }
+}
