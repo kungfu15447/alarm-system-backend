567c7d9d396170f918b9be807e644260b01e3718
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-10-27 09:18:36 +0100

    Implemented local config file for infrastructure project and added gitignore for it

--- infrastructure/SystemContextFactory.cs
+++ infrastructure/SystemContextFactory.cs
@@ -1,15 +1,24 @@
 using System;
+using System.IO;
 using AlarmSystem.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace AlarmSystem.Functions {
     public class SystemContextFactory : IDesignTimeDbContextFactory<SystemContext>
     {
         public SystemContext CreateDbContext(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("infrastructure.config.json", optional: false, reloadOnChange: true);
+            
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("SQL");
             var optionsBuilder = new DbContextOptionsBuilder<SystemContext>();
-            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("SqlConnectionString"));
+            optionsBuilder.UseSqlServer(connectionString);
 
             return new SystemContext(optionsBuilder.Options);
         }
