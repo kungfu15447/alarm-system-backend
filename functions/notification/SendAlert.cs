a61970b4a2ed2108d1a9cb696efe7eeaee2abb39
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-11-09 12:10:36 +0100

    Added NotificationHub nuget package

--- functions/notification/SendAlert.cs
+++ functions/notification/SendAlert.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AlarmSystem.Core.Application;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.NotificationHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -21,8 +23,11 @@ namespace AlarmSystem.Functions.Notfification
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "notify")] HttpRequest req,
             ILogger log)
         {
+            string accessSignature = Environment.GetEnvironmentVariable("DefaultFullSharedAccessSignature");
+            string hubName = Environment.GetEnvironmentVariable("NotificationHubName");
             //TODO Create alarm log
-            
+            NotificationHubClient hub = new NotificationHubClient(accessSignature, hubName);
+
             return new OkResult();
         }
     }
