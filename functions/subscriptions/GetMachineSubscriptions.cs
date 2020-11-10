a2ea1739ab90089f9b39cef9b7f2d3e14ca92679
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-11-10 11:55:41 +0100

    Implemented function

---
+++ functions/subscriptions/GetMachineSubscriptions.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AlarmSystem.Functions.Subscriptions
+{
+    public class GetMachineSubscriptions
+    {
+        public GetMachineSubscriptions()
+        {
+
+        }
+
+        [FunctionName("GetMachineSubscriptions")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "machinesubs")] HttpRequest req,
+            ILogger log)
+        {
+            return new OkResult();
+        }
+    }
+}
