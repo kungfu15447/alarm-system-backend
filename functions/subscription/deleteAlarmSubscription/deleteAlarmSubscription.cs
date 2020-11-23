c90af89ccd13d2104ff5d07a32734e77be329fc1
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-11-23 14:47:17 +0100

    Added function

---
+++ functions/subscription/deleteAlarmSubscription/deleteAlarmSubscription.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AlarmSystem.Functions.Subscription.DeleteAlarmSubscription
+{
+    public class DeleteAlarmSubscription
+    {
+        [FunctionName("SubscribeToAlarm")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "subscribeToAlarm")] HttpRequest req,
+            ILogger log) 
+            {
+                return new NoContentResult();
+            }
+    }
+}
